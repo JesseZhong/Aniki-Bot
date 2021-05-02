6818766fd928a616ca0e236449854bea688c5b6c
Jesse Zhong <jessetakuto@gmail.com>
2021-05-02 07:39:30 -0400

    Added REALLY basic routing.

--- dashboard/server/handler.py
+++ dashboard/server/handler.py
@@ -1,10 +1,39 @@
 #!/bin/python
 
+import json
 from http.server import BaseHTTPRequestHandler
 
 class ServerHandler(BaseHTTPRequestHandler):
 
-    def do_GET(self):
+    def set_headers(self):
         self.send_response(200, 'We good!')
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-Type', 'application/json')
         self.end_headers()
+
+
+    def respond(self, obj: {}):
+        self.wfile.write(
+            json.dumps(obj).encode(encoding='utf_8')
+        )
+
+    def do_GET(self):
+        self.set_headers()
+        
+        routes = {
+            '/personas': self.handle_personas,
+            '/': self.handle_root
+        }
+
+        # Handle a request based off path.
+        if self.path in routes:
+            routes[self.path]()
+
+
+    def handle_root(self):
+        self.respond('Nothing to see here!')
+
+
+    def handle_personas(self):
+        self.respond({
+            'PERSONA': 'YEAAAH BABY'
+        })
