231a9e9b29c0a4470a7024d483b72b8c002b38eb
Jesse Zhong <jessetakuto@gmail.com>
2021-05-02 07:45:17 -0400

    Handle favicon and bad requests.

--- dashboard/server/handler.py
+++ dashboard/server/handler.py
@@ -5,8 +5,8 @@ from http.server import BaseHTTPRequestHandler
 
 class ServerHandler(BaseHTTPRequestHandler):
 
-    def set_headers(self):
-        self.send_response(200, 'We good!')
+    def set_headers(self, code: int, message: str = None):
+        self.send_response(code, message)
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
 
@@ -15,25 +15,35 @@ class ServerHandler(BaseHTTPRequestHandler):
         self.wfile.write(
             json.dumps(obj).encode(encoding='utf_8')
         )
+        
 
     def do_GET(self):
-        self.set_headers()
         
         routes = {
             '/personas': self.handle_personas,
+            '/favicon.ico': self.handle_favicon,
             '/': self.handle_root
         }
 
         # Handle a request based off path.
         if self.path in routes:
             routes[self.path]()
+        else:
+            self.set_headers(404, 'Dude, fuck off!')
+            self.respond('Yo, WTF you doin here?!')
 
 
     def handle_root(self):
+        self.set_headers(200)
         self.respond('Nothing to see here!')
+
+    
+    def handle_favicon(self):
+        return
 
 
     def handle_personas(self):
+        self.set_headers(200)
         self.respond({
             'PERSONA': 'YEAAAH BABY'
         })
