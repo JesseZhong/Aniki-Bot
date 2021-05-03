90ff2e8ce90b1552b134065d2a6a3586c8dbc15a
Jesse Zhong <jessetakuto@gmail.com>
2021-05-03 00:14:59 -0400

    Added Discord auth methods.

--- dashboard/server/basehandler.py
+++ dashboard/server/basehandler.py
@@ -2,9 +2,15 @@
 
 import os
 import json
+import requests
 from http.server import BaseHTTPRequestHandler
 
 DATA_DIR = os.getenv('DATA_DIR')
+SITE_URL = os.getenv('SITE_URL')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+discord_oauth_url = 'https://discord.com/api/oauth2'
 
 class BaseHandler(BaseHTTPRequestHandler):
 
@@ -26,8 +32,70 @@ class BaseHandler(BaseHTTPRequestHandler):
             self.respond(data)
 
     
-    def authenticate(self):
-        return
+    def authorize(self):
+
+        # TODO: Hash session cookie.
+        state=f''
+        scope = 'identity'
+        redirect = f'{SITE_URL}'
+
+        # No need for the user to reapprove.
+        prompt = 'none'
+
+        auth_url = f'{discord_oauth_url}/authorize?response_type=code' + \
+            f'&client_id={CLIENT_ID}&scope={scope}&state={state}' + \
+            f'&redirect_uri={redirect}&prompt={prompt}'
+
+        self.respond({
+            'auth_url': auth_url
+        })
+
+    
+    def request_access(self, code: str):
+        """
+            Responds with a new access token
+            if a valid Discord authorization code.
+        """
+        data = {
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': SITE_URL
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        response = requests.post(
+            f'{discord_oauth_url}/token',
+            data=data,
+            headers=headers
+        )
+        response.raise_for_status()
+        self.respond(response)
+
+
+    def refresh_access(self, refresh_token: str):
+        """
+            Responds with a new access token
+            if a valid Discord refresh token is provided.
+        """
+        data = {
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token
+        }
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        response = requests.post(
+            f'{discord_oauth_url}/token',
+            data=data,
+            headers=headers
+        )
+        response.raise_for_status()
+        self.respond(response)
 
 
     def verify(self):
