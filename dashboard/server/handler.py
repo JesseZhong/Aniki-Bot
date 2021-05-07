682ef624b89cb5854d29a277daaa1680fa1440b8
Jesse Zhong <jessetakuto@gmail.com>
2021-05-07 10:40:55 -0400

    Added buildspec and publish script for site.

--- dashboard/server/handler.py
+++ dashboard/server/handler.py
@@ -7,39 +7,35 @@ from .basehandler import BaseHandler
 class ServerHandler(BaseHandler):
 
     def __init__(self, *args, **kwargs):
-        get_routes = {
-            '/personas': self.handle_personas,
-            '/reactions': self.handle_reactions,
-            '/favicon.ico': self.handle_favicon,
-            '/': self.handle_root
-        }
-
-        post_routes = {
-
-        }
-
         super().__init__(
-            get_routes,
-            post_routes,
+            get_routes={
+                '/personas': self.get_personas,
+                '/reactions': self.get_reactions,
+                '/favicon.ico': self.get_favicon,
+                '/': self.get_root
+            },
+            post_routes={},
+            put_routes={},
+            delete_routes={},
             *args,
             **kwargs
         )
 
 
-    def handle_root(self):
+    def get_root(self):
         self.set_headers(200)
         self.respond('Nothing to see here!')
 
 
-    def handle_favicon(self):
+    def get_favicon(self):
         return
         
 
-    def handle_personas(self):
+    def get_personas(self):
         self.set_headers(200)
         self.send_file('personas.json')
 
 
-    def handle_reactions(self):
+    def get_reactions(self):
         self.set_headers(200)
         self.send_file('reactions.json')
